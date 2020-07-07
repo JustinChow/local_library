16b8142780e4d218d9fb2af0195fea9a41cea863
Justin Chow <justin.chow@queensu.ca>
2020-07-07 18:32:11 -0400

    Adding optional environment variable for mongodb URL for deploying to production

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 
 // set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = config.mongodb_url;
+var mongoDB = process.env.MONGODB_URI || config.mongodb_url;
 mongoose.connect(mongoDB, { UseNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
